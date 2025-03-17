65a1aa07c64574cf84286be1e61c658ad26fdfd4
J3m3 <y.j3ms.n@gmail.com>
2025-03-18 04:31:01 +0900

    Add script to create a bootable disk
    Note: This should be run inside the container.

---
+++ mkdisk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xe
+
+cp /linux/arch/arm64/boot/Image.gz .
+
+dd if=/dev/zero of=disk.img bs=1M count=36
+mkfs.vfat -F32 -n EFI disk.img
+mkdir -p mnt
+mount disk.img mnt
+grub-install --target=arm64-efi --efi-directory=mnt --boot-directory=mnt --removable
+
+grub_cfg_path="mnt/EFI/BOOT/grub.cfg"
+
+echo "set timeout=10" > "$grub_cfg_path"
+echo "set default=0" >> "$grub_cfg_path"
+echo "menuentry \"Linux Kernel\" {" >> "$grub_cfg_path"
+echo "    linux /boot/Image.gz console=ttyAMA0" >> "$grub_cfg_path"
+echo "    initrd /boot/initramfs.cpio" >> "$grub_cfg_path"
+echo "}" >> "$grub_cfg_path"
+
+mkdir mnt/boot
+cp Image.gz initramfs.cpio mnt/boot
+umount mnt
