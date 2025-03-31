96b41da6d2e6c1ea92a79c041d710b6c8de43711
J3m3 <y.j3ms.n@gmail.com>
2025-03-31 18:26:31 +0900

    Fix path to disk image

--- boot.sh
+++ boot.sh
@@ -31,7 +31,7 @@ elif [ "$1" == "--image" ]; then
     -m 1G \
     -drive if=pflash,format=raw,file=eficode.img,readonly=on \
     -drive if=pflash,format=raw,file=efivars.img \
-    -drive file=distro/disk.img,format=raw,if=virtio \
+    -drive file=disk.img,format=raw,if=virtio \
     -nographic
 else
   print_help
