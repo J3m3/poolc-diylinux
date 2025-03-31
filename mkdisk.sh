55f3f419d8641b00c824f66998fa637189487d8b
J3m3 <y.j3ms.n@gmail.com>
2025-03-31 16:39:22 +0900

    Use rEFInd instead of GRUB
    I cannot explain why GRUB allows linux to boot as EFI stub. This
    may be due to EFI handover protocol, but it's deprecated...
    
    Since rEFInd is clearly a boot manager (not a boot loader), I can
    explain this stuff better.

--- mkdisk.sh
+++ mkdisk.sh
@@ -2,30 +2,53 @@
 
 set -e
 
-trap "umount mnt 2>/dev/null || true; exit 1" ERR INT TERM
-
 if [[ "$(pwd)" != "/distro" ]]; then
   echo "Error: This script must be executed from /distro"
   exit 1
 fi
 
-cp /linux/arch/arm64/boot/Image.gz .
+img="disk.img"
+block_dev=""
+mount_point="mnt"
 
-dd if=/dev/zero of=disk.img bs=1M count=36
-mkfs.vfat -F32 -n EFI disk.img
-mkdir -p mnt
-mount disk.img mnt
-grub-install --target=arm64-efi --efi-directory=mnt --boot-directory=mnt --removable
+cleanup() {
+  if [[ -n $block_dev ]]; then
+    losetup -d $block_dev
+  fi
+  if [[ -d $mount_point ]]; then
+    umount --all-targets $mount_point 2>/dev/null
+    rmdir $mount_point 2>/dev/null
+  fi
+  rm -f $img
+  exit 1
+}
 
-grub_cfg_path="mnt/EFI/BOOT/grub.cfg"
+trap cleanup INT TERM ERR
 
-echo "set timeout=10" > "$grub_cfg_path"
-echo "set default=0" >> "$grub_cfg_path"
-echo "menuentry \"Linux Kernel\" {" >> "$grub_cfg_path"
-echo "    linux /boot/Image.gz console=ttyAMA0" >> "$grub_cfg_path"
-echo "    initrd /boot/initramfs.cpio" >> "$grub_cfg_path"
-echo "}" >> "$grub_cfg_path"
+cp /linux/arch/arm64/boot/Image .
 
-mkdir mnt/boot
-cp Image.gz initramfs.cpio mnt/boot
-umount mnt
+dd if=/dev/zero of=$img bs=1M count=36
+block_dev=$(losetup -f --show disk.img)
+mkfs.vfat -F32 $block_dev
+
+refind-install --usedefault $block_dev
+
+mkdir -p $mount_point
+mount $block_dev $mount_point
+
+mv $mount_point/EFI/BOOT/refind_aa64.efi $mount_point/EFI/BOOT/BOOTAA64.efi
+
+cp Image initramfs.cpio $mount_point/EFI/BOOT
+
+CFG_PATH="mnt/EFI/BOOT/refind.conf"
+echo "timeout 10" > $CFG_PATH
+echo "textonly" >> $CFG_PATH
+echo "default_selectin 1" >> $CFG_PATH
+echo "menuentry \"Custom Linux\" {" >> $CFG_PATH
+echo "loader /EFI/BOOT/Image" >> $CFG_PATH
+echo "initrd /EFI/BOOT/initramfs.cpio" >> $CFG_PATH
+echo "options \"console=ttyAMA0\"" >> $CFG_PATH
+echo "}" >> $CFG_PATH
+
+losetup -d $block_dev
+umount --all-target $mount_point
