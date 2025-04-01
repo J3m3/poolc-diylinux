c9e153c59a1c3f55287fc647d7c20ccc8f95d88f
J3m3 <y.j3ms.n@gmail.com>
2025-04-01 23:39:41 +0900

    Add userspace system call test code

---
+++ syscall_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct silly_info {
+  int nice;
+  void *start_heap;
+  void *end_heap;
+  char comm[16];
+} SillyInfo;
+
+int main(int argc, char **argv) {
+  SillyInfo info = {0};
+  long r = syscall(467, argc < 2 ? 1 : atoi(argv[1]), &info);
+  if (r < 0) {
+    fprintf(stderr, "Errno: %ld\n", r);
+    perror("Error");
+  }
+
+  printf("nice: %d\n", info.nice);
+  printf("start_heap: %p\n", info.start_heap);
+  printf("end_heap: %p\n", info.end_heap);
+  printf("command: %s\n", info.comm);
+
+  return 0;
+}
