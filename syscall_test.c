8e5d416d9ce60dde17b5f91f77c8916fc8cfe264
J3m3 <y.j3ms.n@gmail.com>
2025-04-03 09:05:15 +0900

    Use macros to name constants

--- syscall_test.c
+++ syscall_test.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define TASK_COMM_LEN 16
+#define SYS_silly 467
+
 typedef struct silly_info {
   int nice;
   void *start_heap;
   void *end_heap;
-  char comm[16];
+  char comm[TASK_COMM_LEN];
 } SillyInfo;
 
 int main(int argc, char **argv) {
   SillyInfo info = {0};
-  long r = syscall(467, argc < 2 ? getpid() : atoi(argv[1]), &info);
+  long r = syscall(SYS_silly, argc < 2 ? getpid() : atoi(argv[1]), &info);
   if (r < 0) {
     fprintf(stderr, "Errno: %ld\n", r);
     perror("Error");
